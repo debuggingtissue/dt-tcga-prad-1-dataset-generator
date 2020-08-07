e7ab24782a579602618256db86a51eecba23ccd5
debuggingtissue <debugger@debuggingtissue.com>
2020-08-07 23:14:53 +0200

    remove parser bug in image patch file name builder

--- src/utils/image_patch_file_name_builder.py
+++ src/utils/image_patch_file_name_builder.py
@@ -2,8 +2,8 @@ from .image_patch_file_name_constants import *
 from .image_patch_predictions_constants import *
 
 def build_image_patch_file_name(case_id, resolution_level, x_coordinate, y_coordinate, width, height):
-    return CASE_ID + EQUAL + case_id + SEPARATOR + RESOLUTION_LEVEL + EQUAL + str(
-        resolution_level) + SEPARATOR + X_COORDINATE + EQUAL + str(
+    return CASE_ID + EQUAL + case_id + SEPARATOR + RESOLUTION_LEVEL + EQUAL + str(int(
+        resolution_level)) + SEPARATOR + X_COORDINATE + EQUAL + str(
         x_coordinate) + SEPARATOR + Y_COORDINATE + EQUAL + str(y_coordinate) + SEPARATOR + WIDTH + EQUAL + str(
         width) + SEPARATOR + HEIGHT + EQUAL + str(height) + JPEG_ENDING
 
