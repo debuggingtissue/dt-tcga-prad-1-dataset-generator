6865dca71c997ba172d0961a42437f965328e8eb
debuggingtissue <debugger@debuggingtissue.com>
2020-08-20 19:24:45 +0200

    add auto resume feature WIP

--- src/utils/auto_resume_utils.py
+++ src/utils/auto_resume_utils.py
@@ -1,5 +1,5 @@
 
-def is_case_already_classified_into_csv(input_case_folder_to_predictor_path, predicted_cases_csv_folder_path):
+def is_case_already_classified_into_output_csv(input_case_folder_to_predictor_path, predicted_cases_csv_folder_path):
 
     case_name = input_case_folder_to_predictor_path.split()[-1]
     predicted_csv_for_case_path = predicted_cases_csv_folder_path + case_name + ".csv"
@@ -12,14 +12,20 @@ def is_case_already_classified_into_csv(input_case_folder_to_predictor_path, pre
     csv_file = open(predicted_csv_for_case_path)
     reader = csv.reader(csv_file)
     lines_in_csv = len(list(reader))
-    input_files = path_utils.create_full_paths_to_files_in_directory_path
+    input_files = path_utils.create_full_paths_to_files_in_directory_path(input_case_folder_to_predictor_path)
 
     # get image_count_of input folder open the csv and see if number matches; no: do classification
     if lines_in_csv is input_files.count():
+        return True
+    return False
+
+def is_case_already_classified_into_output_directory(input_directory_path, output_directory_path):
+    output_directory_path = path_utils.does_path_exist(output_directory_path)
+    if not output_directory_path:
         return False
-    return True
-
-
-
-
+    input_file_paths = path_utils.create_full_paths_to_files_in_directory_path(input_directory_path)
+    output_file_paths = path_utils.create_full_paths_to_files_in_directory_path(output_directory_path)
+    if input_file_paths.count() is output_file_paths.count():
+        return True
+    return False
 
