184bb81da26a2521c87be9ef9a39b039416f6f54
debuggingtissue <debugger@debuggingtissue.com>
2020-08-22 14:51:51 +0200

    comment run script

--- src/run.sh
+++ src/run.sh
@@ -5,20 +5,20 @@ source config.sh
 #               RUN               #
 ###################################
 
-source "${S0_VIRTUAL_ENV_36}/bin/activate"
-python3.6 1_split_svs_images_to_image_patches.py \
-  -i $S1_SPLIT_SVS_S0_INPUT_DIRECTORY_PATH \
-  -o $S1_SPLIT_SVS_OUTPUT_DIRECTORY_PATH \
-  -r $S1_RESOLUTION_LEVEL \
-  -op $S1_OVERLAP_PERCENTAGE \
-  -ws $S1_WINDOW_SIZE
-#
-python3.6 2_preprocess_image_patches.py \
-  -i $S2_PREPROCESS_IMAGE_PATCHES_INPUT_DIRECTORY_PATH \
-  -o $S2_PREPROCESS_IMAGE_PATCHES_OUTPUT_DIRECTORY_PATH \
-  -fc $S2_FIRST_CENTERMOST_CROP_SIZE \
-  -ds $S2_DOWNSCALED_SIZE \
-  -sc $S2_SECOND_CENTERMOST_CROP_SIZE
+#source "${S0_VIRTUAL_ENV_36}/bin/activate"
+#python3.6 1_split_svs_images_to_image_patches.py \
+#  -i $S1_SPLIT_SVS_S0_INPUT_DIRECTORY_PATH \
+#  -o $S1_SPLIT_SVS_OUTPUT_DIRECTORY_PATH \
+#  -r $S1_RESOLUTION_LEVEL \
+#  -op $S1_OVERLAP_PERCENTAGE \
+#  -ws $S1_WINDOW_SIZE
+##
+#python3.6 2_preprocess_image_patches.py \
+#  -i $S2_PREPROCESS_IMAGE_PATCHES_INPUT_DIRECTORY_PATH \
+#  -o $S2_PREPROCESS_IMAGE_PATCHES_OUTPUT_DIRECTORY_PATH \
+#  -fc $S2_FIRST_CENTERMOST_CROP_SIZE \
+#  -ds $S2_DOWNSCALED_SIZE \
+#  -sc $S2_SECOND_CENTERMOST_CROP_SIZE
 ##rm -r $S2_DIRECTORY_TO_DELETE
 ##
 source "${S0_VIRTUAL_ENV_27}/bin/activate"
